02eb78543f8b0f132fea500f51a61d5c1e8c6513
David Justice <david@devigned.com>
2016-01-13 09:43:09 -0800

    first commit for ref doc generation

---
+++ README.md
@@ -0,0 +1,13 @@
+# Azure Reference Documentation
+This repo generates and publishes documentation for azure repositories. It's currently in progress...
+
+The project expects the collection of repos to be setup with `repo init -u https://github.com/azure/ref-docs`. 
+
+## Getting started
+- `git clone https://github.com/azure/ref-docs`
+- `npm install`
+
+## Current Commands
+- `gulp --tasks # to see all of the tasks`
+- `gulp build # to build the dist directory`
+- etc... read the gulpfile
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+del = require('del'),
+shell = require('gulp-shell'),
+ghPages = require('gulp-gh-pages'),
+argv = require('yargs').argv,
+gulpif = require('gulp-if');
+
+/// Repo initialiazation, syncronization and cleanup
+gulp.task('sync', ['init'], shell.task('repo sync --no-tags -c'));
+gulp.task('init', shell.task('repo init -u https://github.com/devigned/azure-manifest'));
+gulp.task('clean', function(){
+    return del(['./.repo', './azure', './dist', './.publish']);
+});
+
+/// Javadoc generation and publication
+gulp.task('java:pom', ['sync'], function(){
+   return gulp.src('./src/pom.xml').pipe(gulp.dest('./azure/java')); 
+});
+gulp.task('java:build', ['java:pom'], shell.task('mvn package javadoc:aggregate -DskipTests=true -q', {cwd: './azure/java'}));
+gulp.task('java:stage', ['java:build'], function(){
+    return gulp.src('./azure/java/target/site/apidocs/**/*').pipe(gulp.dest('./dist/java')); 
+});
+
+/// Top level build entry point
+gulp.task('build', ['java:build', 'node:build']);
+gulp.task('publish', ['build'], function(){
+    return gulp.src('./dist/**/*').pipe(gulpif(!argv.dryrun, ghPages()));
+});
+
+gulp.task('default', ['publish']);
