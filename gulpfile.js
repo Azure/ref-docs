192140f6cf852535a850a68e747b60ad47ec4d1c
Hovsep Mkrtchyan <hovsepm@microsoft.com>
2018-02-20 17:44:55 -0800

    changed spawn to exec in the gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,15 @@ var shell = require('gulp-shell');
 var ghPages = require('gulp-gh-pages');
 var argv = require('yargs').argv;
 var gulpif = require('gulp-if');
-var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
 
 // Pull doc db private repo v.1.15.0
 gulp.task('docdb', function () {
     /*shell.task('git clone https://' + process.env.GH_TOKEN + ':x-oauth-basic@github.com/azure/azure-documentdb-java-pr.git documentdb', {cwd: './azure/java'})*/
-    spawn('mkdir -p ./azure/java', [], { shell: true, stdio: "inherit" });
-    spawn('git clone https://' + process.env.GH_TOKEN + ':x-oauth-basic@github.com/azure/azure-documentdb-java-pr.git documentdb', [], { cwd: './azure/java', shell: true, stdio: "inherit" });
-    spawn('git checkout -b 1.15.0 1.15.0', [], { cwd: './azure/java/documentdb', shell: true, stdio: "inherit" });
+    exec('mkdir -p ./azure/java', { cwd: '.'}, function(err, stdout, stderr) {
+    	exec('git clone https://' + process.env.GH_TOKEN + ':x-oauth-basic@github.com/azure/azure-documentdb-java-pr.git documentdb', { cwd: './azure/java'}, function(err, stdout, stderr) {
+    		exec('git checkout -b 1.15.0 1.15.0', { cwd: './azure/java/documentdb'});
+	});
 });
 
 /// Repo initialiazation, syncronization and cleanup
@@ -19,7 +20,7 @@ gulp.task('init', ['docdb'], shell.task('repo init -u https://github.com/azure/r
 gulp.task('clean', function(){
     return del(['./.repo', './azure', './dist', './.publish']);
 });
-gulp.task('sync', ['init'], shell.task('repo sync'));
+gulp.task('sync', ['init'], shell.task('repo sync --no-tags -c'));
 
 /// Javadoc generation and publication
 gulp.task('java:pom', ['sync'], function(){
