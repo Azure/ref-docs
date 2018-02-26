7d4e7790a7c81d681b62f6f0828091ccdebc4ae3
Hovsep <hovsepm@microsoft.com>
2018-02-26 14:45:00 -0800

    Cleaned up configuration for Fluent 1.6 (#5)
    * Added exclusion to pom.xml
    
    * Added Batch data plane
    
    * Fixed documentDB
    
    * Fixed rollup package
    
    * moved DocumentDB clone to a separate gulp task
    
    * Added tags for the sources
    
    * Fixed revision tags
    
    * removed no-tag clone
    
    * changed spawn to exec in the gulpfile.js
    
    * Fixed closing brackets
    
    * Cleaned up javadocs
    
    * final fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,28 @@
-var gulp = require('gulp'),
-del = require('del'),
-shell = require('gulp-shell'),
-ghPages = require('gulp-gh-pages'),
-argv = require('yargs').argv,
-gulpif = require('gulp-if');
+var gulp = require('gulp');
+var del = require('del');
+var shell = require('gulp-shell');
+var ghPages = require('gulp-gh-pages');
+var argv = require('yargs').argv;
+var gulpif = require('gulp-if');
+var exec = require('child_process').exec;
 
-/// Repo initialiazation, syncronization and cleanup
-gulp.task('sync', ['init'], shell.task('repo sync --no-tags -c'));
-gulp.task('init', shell.task('repo init -u https://github.com/azure/ref-docs'));
 gulp.task('clean', function(){
     return del(['./.repo', './azure', './dist', './.publish']);
 });
 
+/// Repo initialiazation, syncronization and cleanup
+gulp.task('init', shell.task('repo init -u https://github.com/azure/ref-docs'));
+gulp.task('sync', ['init'], shell.task('repo sync --no-tags -c'));
+
+// Pull doc db private repo v.1.15.0
+gulp.task('docdb', ['sync'], function () {
+	exec('git clone https://' + process.env.GH_TOKEN + ':x-oauth-basic@github.com/azure/azure-documentdb-java-pr.git documentdb', { cwd: './azure/java'}, function(err, stdout, stderr) {
+		exec('git checkout -b 1.15.0 1.15.0', { cwd: './azure/java/documentdb'});
+	});
+});
+
 /// Javadoc generation and publication
-gulp.task('java:pom', ['sync'], function(){
+gulp.task('java:pom', ['sync', 'docdb'], function(){
    return gulp.src('./src/pom.xml').pipe(gulp.dest('./azure/java')); 
 });
 gulp.task('java:build', ['java:pom'], shell.task('mvn package javadoc:aggregate -DskipTests=true -q', {cwd: './azure/java'}));
