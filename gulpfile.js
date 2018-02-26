ef153385c1d650d7506ced705e91b12e35da600e
Hovsep Mkrtchyan <hovsepm@microsoft.com>
2018-02-26 14:30:55 -0800

    final fix

--- gulpfile.js
+++ gulpfile.js
@@ -6,25 +6,23 @@ var argv = require('yargs').argv;
 var gulpif = require('gulp-if');
 var exec = require('child_process').exec;
 
-// Pull doc db private repo v.1.15.0
-gulp.task('docdb', function () {
-    /*shell.task('git clone https://' + process.env.GH_TOKEN + ':x-oauth-basic@github.com/azure/azure-documentdb-java-pr.git documentdb', {cwd: './azure/java'})*/
-    exec('mkdir -p ./azure/java', { cwd: '.'}, function(err, stdout, stderr) {
-    	exec('git clone https://' + process.env.GH_TOKEN + ':x-oauth-basic@github.com/azure/azure-documentdb-java-pr.git documentdb', { cwd: './azure/java'}, function(err, stdout, stderr) {
-    		exec('git checkout -b 1.15.0 1.15.0', { cwd: './azure/java/documentdb'});
-		});
-	});
-});	
-
-/// Repo initialiazation, syncronization and cleanup
-gulp.task('init', ['docdb'], shell.task('repo init -u https://github.com/azure/ref-docs'));
 gulp.task('clean', function(){
     return del(['./.repo', './azure', './dist', './.publish']);
 });
+
+/// Repo initialiazation, syncronization and cleanup
+gulp.task('init', shell.task('repo init -u https://github.com/azure/ref-docs'));
 gulp.task('sync', ['init'], shell.task('repo sync --no-tags -c'));
 
+// Pull doc db private repo v.1.15.0
+gulp.task('docdb', ['sync'], function () {
+	exec('git clone https://' + process.env.GH_TOKEN + ':x-oauth-basic@github.com/azure/azure-documentdb-java-pr.git documentdb', { cwd: './azure/java'}, function(err, stdout, stderr) {
+		exec('git checkout -b 1.15.0 1.15.0', { cwd: './azure/java/documentdb'});
+	});
+});
+
 /// Javadoc generation and publication
-gulp.task('java:pom', ['sync'], function(){
+gulp.task('java:pom', ['sync', 'docdb'], function(){
    return gulp.src('./src/pom.xml').pipe(gulp.dest('./azure/java')); 
 });
 gulp.task('java:build', ['java:pom'], shell.task('mvn package javadoc:aggregate -DskipTests=true -q', {cwd: './azure/java'}));
